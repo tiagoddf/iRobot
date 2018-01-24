e0fe6e4d007416f4affefc88ad5dfaafa7ce429d
tiagoddf <33496838+tiagoddf@users.noreply.github.com>
2018-01-24 11:17:01 -0200

    1.1.9

--- src/iRobot/iRobot_Functions.java
+++ src/iRobot/iRobot_Functions.java
@@ -16,6 +16,16 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
+import java.io.IOException;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author tduarte
@@ -118,5 +128,36 @@ public class iRobot_Functions {
             Logger.getLogger(iRobot_Functions.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+  }
+  // Email Sender
+  public void SendMail(String username, String password, String title, String msg, String reciver) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "outlook.office365.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(reciver));
+            message.setSubject(title);
+            
+            
+            message.setContent(msg, "text/html; charset=utf-8");
+            
+            Transport.send(message);
+            System.out.println("Done");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
   }
 }
